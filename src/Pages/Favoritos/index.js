17db53b76b8e97fa9ea0f78ea97f7782d6a37d4d
Yan Gomes <yangomesmd@gmail.com>
2024-11-22 16:07:30 -0400

    adicionando favoritos e telas adicionais 2

--- src/Pages/Favoritos/index.js
+++ src/Pages/Favoritos/index.js
@@ -1,47 +1,44 @@
-import { useEffect, useState } from "react"
-import "./favoritos.css"
-import { Link } from "react-router-dom"
-import { toast } from "react-toastify"
+import { useEffect, useState } from "react";
+import "./favoritos.css";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
-export default function Favoritos(){
+export default function Favoritos() {
+  const [filmes, setFilmes] = useState([]);
 
-    const [filmes, setFilmes] = useState([])
+  useEffect(() => {
+    let lista = localStorage.getItem("@primeflix");
+    setFilmes(JSON.parse(lista) || []);
+  }, []);
 
-    useEffect(()=>{
-        let lista = localStorage.getItem("@primeflix")
-        setFilmes(JSON.parse(lista)|| [])
-    },[])
+  function ExcluirFilme(id) {
+    let filtro = filmes.filter((item) => {
+      return item.id !== id;
+    });
+    setFilmes(filtro);
+    localStorage.setItem("@primeflix", JSON.stringify(filtro));
+    toast.success("Filme removido");
+  }
 
-    function ExcluirFilme(id){
-        let filtro = filmes.filter((item)=>{
-            return (item.id !== id)
-        })
-        setFilmes(filtro)
-        localStorage.setItem("@primeflix", JSON.stringify(filtro))
-        toast.success("Filme removido")
-    }
+  return (
+    <div className="meus-filmes">
+      <h1>Meus filmes</h1>
 
+      {filmes.length === 0 && <span>Você não possui filmes salvos :(</span>}
 
-
-    return(
-        <div className="meus-filmes">
-            <h1 >Meus filmes</h1>
-
-            {filmes.length===0 && <span>Você não possui filmes salvos :(</span>}
-
-            <ul>
-                {filmes.map((item)=>{
-                    return(
-                        <li key={item}>
-                            <span>{item.title}</span>
-                            <div>
-                                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button onClick={()=>ExcluirFilme(item.id)}>Excluir</button>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+      <ul>
+        {filmes.map((item) => {
+          return (
+            <li key={item}>
+              <span>{item.title}</span>
+              <div>
+                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                <button onClick={() => ExcluirFilme(item.id)}>Excluir</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import RouteApp from './Routes';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RouteApp from "./Routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ToastContainer autoClose={3000}/>
-    <RouteApp/>
+    <ToastContainer autoClose={3000} />
+    <RouteApp />
   </React.StrictMode>
 );
 
- // const [nutri, setNutri] = useState([]);
-  //  useEffect(() => {
-  //  function loadApi() {
-  //  let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
-  //fetch(url)
-  //.then((r) => r.json())
-  //    .then((json) => {
-  //    setNutri(json);
-  // });
-  // }
-  //loadApi();
-  //}, []);
